ce6a9d615ccc0985a92f44331868044793e44da6
MuhammadSalmanSiddiqui <salmansidd991@gmail.com>
2021-12-26 21:30:35 +0500

    category controller

---
+++ src/controllers/category.js
@@ -0,0 +1,45 @@
+import category from '../models/category';
+
+export const createCategory = async (req, res) => {
+  try {
+    const newCategory = await category.create(req.body);
+    return res.status(200).json({
+      message: 'Category created successfully',
+      data: newCategory,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Internal Server Error. Please try again later.',
+      error: error,
+    });
+  }
+};
+
+export const getCategory = async (req, res) => {
+  try {
+    const categories = await category.find(req.query);
+    return res.json({
+      message: 'Gathered all categories!',
+      data: categories,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Internal Server Error. Please try again later.',
+      error: error,
+    });
+  }
+};
+
+export const deleteCategories = async (req, res) => {
+  try {
+    await category.deleteOne({ _id: req.body.id });
+    return res.json({
+      message: 'Category deleted!',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Internal Server Error. Please try again later.',
+      error: error,
+    });
+  }
+};
