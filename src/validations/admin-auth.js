6a6895d0c111093ec7a2861b7cdbfe20a2299651
MuhammadSalmanSiddiqui <salmansidd991@gmail.com>
2022-01-06 03:26:54 +0500

    update

--- src/validations/admin-auth.js
+++ src/validations/admin-auth.js
@@ -4,7 +4,6 @@ export const registerSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  cpassword: joi.ref('password'),
 });
 export const loginSchema = joi.object({
   email: joi.string().email().required(),
